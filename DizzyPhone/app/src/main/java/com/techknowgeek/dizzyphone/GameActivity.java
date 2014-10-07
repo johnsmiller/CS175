3b273b6782d6f62de91bfc73b947ebcb717598d4
Mark Odell <dracone.cordis@gmail.com>
2014-10-07 16:51:25 -0700

    Moved variables for cscores and other stuff into mainmenu screen to be loaded when app is loaded and STUFF

--- DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/GameActivity.java
+++ DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/GameActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -12,17 +13,15 @@ import java.util.TimerTask;
 
 public class GameActivity extends Activity {
 
-    static final String STATE_TIME = "time";
-    static final String STATE_PLAYER_NAME = "playerName";
-    static final String STATE_HIGH_SCORE = "highScore";
-    static final String STATE_SCORE = "playerScore";
-    static final String STATE_LIVES = "lives";
+    private static TextView highScoreView;
+    private static TextView playerScoreView;
+    private static TextView livesView;
 
-    private static double time;
-    private static String playerName;
-    private static int highScore;
-    private static int playerScore;
-    private static int lives;
+    private double time;
+    private String playerName;
+    private int highScore;
+    private int playerScore;
+    private int lives;
 
     private Timer zehTimer;
 
@@ -33,22 +32,10 @@ public class GameActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.hide();
 
-        // Check whether we're recreating a previously destroyed instance
-        if (savedInstanceState != null) {
-            // Restore value of members from saved state
-            time = savedInstanceState.getInt(STATE_TIME);
-            playerName = savedInstanceState.getString(STATE_PLAYER_NAME);
-            highScore = savedInstanceState.getInt(STATE_HIGH_SCORE);
-            playerScore = savedInstanceState.getInt(STATE_SCORE);
-
-        } else {
-            // Probably should initialize members with default values for a new instance
-            time = 1.0;
-            playerName = "Player 1";
-            highScore = 0;
-            playerScore = 0;
-            lives = 3; // Start with three lives
-        }
+        // Create TextView Classes
+        highScoreView = (TextView)findViewById(R.id.currentHighScore);
+        playerScoreView = (TextView)findViewById(R.id.currentScore);
+        livesView = (TextView)findViewById(R.id.currentLives);
 
         //zehTimer = new TimerTask();
         setContentView(R.layout.activity_game);
@@ -74,22 +61,10 @@ public class GameActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save the user's current game state
-        savedInstanceState.putDouble(STATE_TIME, time);
-        savedInstanceState.putString(STATE_PLAYER_NAME, playerName);
-        savedInstanceState.putInt(STATE_HIGH_SCORE, highScore);
-        savedInstanceState.putInt(STATE_SCORE, playerScore);
-        savedInstanceState.putInt(STATE_LIVES, lives);
-
-        // Always call the superclass so it can save the view hierarchy state
-        super.onSaveInstanceState(savedInstanceState);
-    }
-
     public void onButtonClick(){
         // Kill timer
         // Score++;
+        // Update Scores
     }
 
     public boolean timeIsUp(){
@@ -99,32 +74,13 @@ public class GameActivity extends Activity {
         //    Add score to highscore table
         // ELSE
         //    Lives--;
+        // Update Scores
         return true;
     }
 
+    public void updateScores(){
 
-    public static void setStateTime(double timeState)
-    {
-        time = timeState;
     }
 
-    public static void setStatePlayerName(String nameState)
-    {
-        playerName = nameState;
-    }
 
-    private static void setStateScore(int scoreState)
-    {
-        playerScore = scoreState;
-    }
-
-    private static void setStateHighScore(int scoreHighState)
-    {
-        highScore = scoreHighState;
-    }
-
-    private static void setStateLives(int livesState)
-    {
-        lives = livesState;
-    }
 }
--- DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/menuscreenActivity.java
+++ DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/menuscreenActivity.java
@@ -13,6 +13,18 @@ import android.view.View;
  */
 public class menuscreenActivity extends Activity {
 
+    static final String STATE_TIME = "time";
+    static final String STATE_PLAYER_NAME = "playerName";
+    static final String STATE_HIGH_SCORE = "highScore";
+    static final String STATE_SCORE = "playerScore";
+    static final String STATE_LIVES = "lives";
+
+    private static double time;
+    private static String playerName;
+    private static int highScore;
+    private static int playerScore;
+    private static int lives;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +32,80 @@ public class menuscreenActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.hide();
 
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            time = savedInstanceState.getInt(STATE_TIME);
+            playerName = savedInstanceState.getString(STATE_PLAYER_NAME);
+            highScore = savedInstanceState.getInt(STATE_HIGH_SCORE);
+            playerScore = savedInstanceState.getInt(STATE_SCORE);
+
+        } else {
+            // Probably should initialize members with default values for a new instance
+            time = 1.0;
+            playerName = "Player 1";
+            highScore = 0;
+            playerScore = 0;
+            lives = 3; // Start with three lives
+        }
+
         setContentView(R.layout.activity_menuscreen);
 
         final View contentView = findViewById(R.id.fullscreen_content);
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putDouble(STATE_TIME, time);
+        savedInstanceState.putString(STATE_PLAYER_NAME, playerName);
+        savedInstanceState.putInt(STATE_HIGH_SCORE, highScore);
+        savedInstanceState.putInt(STATE_SCORE, playerScore);
+        savedInstanceState.putInt(STATE_LIVES, lives);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public static double getTime() {
+        return time;
+    }
+
+    public static void setTime(double time) {
+        menuscreenActivity.time = time;
+    }
+
+    public static String getPlayerName() {
+        return playerName;
+    }
+
+    public static void setPlayerName(String playerName) {
+        menuscreenActivity.playerName = playerName;
+    }
+
+    public static int getHighScore() {
+        return highScore;
+    }
+
+    public static void setHighScore(int highScore) {
+        menuscreenActivity.highScore = highScore;
+    }
+
+    public static int getPlayerScore() {
+        return playerScore;
+    }
+
+    public static void setPlayerScore(int playerScore) {
+        menuscreenActivity.playerScore = playerScore;
+    }
+
+    public static int getLives() {
+        return lives;
+    }
+
+    public static void setLives(int lives) {
+        menuscreenActivity.lives = lives;
     }
 
     public void sendGameActivity(View view)
