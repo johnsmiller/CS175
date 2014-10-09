d6cf8f5cc0b99800246017b89946fa75e26afc48
johnsmiller <computerman790@gmail.com>
2014-10-09 13:01:34 -0700

    First attempt at game play functions

--- DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/GameActivity.java
+++ DizzyPhone/app/src/main/java/com/techknowgeek/dizzyphone/GameActivity.java
@@ -1,8 +1,14 @@
 package com.techknowgeek.dizzyphone;
 
+import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -16,12 +22,16 @@ public class GameActivity extends Activity {
     private static TextView highScoreView;
     private static TextView playerScoreView;
     private static TextView livesView;
+    private static TextView currentOrientation;
+    private static TextView fullScreenView;
 
     private long time;
     private String playerName;
     private static int highScore;
     private static int playerScore;
     private static int lives;
+
+    private static boolean isPortrait; //True if requested orientation is portrait
 
     private Timer zehTimer;
 
@@ -37,6 +47,8 @@ public class GameActivity extends Activity {
         highScoreView = (TextView)findViewById(R.id.currentHighScore);
         playerScoreView = (TextView)findViewById(R.id.currentScore);
         livesView = (TextView)findViewById(R.id.currentLives);
+        currentOrientation = (TextView) findViewById(R.id.currentOrientation);
+        fullScreenView = (TextView) findViewById(R.id.fullscreen_content);
 
         // Assign Vars and Get Highscore
         highScore = menuscreenActivity.getHighScore();
@@ -45,7 +57,7 @@ public class GameActivity extends Activity {
         lives = 3;
 
         upDateTextViews();
-
+        randomOrientation();
         setTimerTask();
     }
 
@@ -64,6 +76,7 @@ public class GameActivity extends Activity {
             }
         };
         zehTimer = new Timer();
+        fullScreenView.setBackgroundColor(0x0099cc);
         zehTimer.schedule(aTask, time);
     }
 
@@ -88,15 +101,45 @@ public class GameActivity extends Activity {
     }
 
     public void onButtonClick(){
-        // Kill timer
-        playerScore++;
-        // Update Scores Views
-        playerScoreView.setText(String.valueOf(playerScore));
+        if(isCorrectOrientation()) {
+            zehTimer.cancel();
+            playerScore++;
+            upDateTextViews();
+            randomOrientation();
+            setTimerTask();
+        }
+    }
+
+    private void randomOrientation()
+    {
+        if(Math.random() >= .5)
+        {
+            //portrait
+            isPortrait=true;
+            currentOrientation.setText("Portrait");
+        }
+        else {
+            //landscape
+            isPortrait=false;
+            currentOrientation.setText("Landscape");
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private boolean isCorrectOrientation()
+    {
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){ //Portrait
+            return isPortrait;
+        }else { //Landscape
+            return !isPortrait;
+        }
+
     }
 
     private void timeIsUp(){
         // Flash background color to notify user.
         // Its not required but might make the app easier
+        fullScreenView.setBackgroundColor(Color.RED);
 
         if(lives <= 0) {
             // IF playerScore > highScore THEN update highScore to playerScore in menuscreen
@@ -104,6 +147,8 @@ public class GameActivity extends Activity {
             this.finish();
         } else {
             lives--;
+            randomOrientation();
+            upDateTextViews();
             setTimerTask();
             // Update Scores Views
         }
