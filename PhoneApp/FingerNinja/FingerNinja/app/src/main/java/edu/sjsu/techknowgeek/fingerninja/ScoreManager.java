e1fb38f68ab799af8bc9c1cee41791c5b2709491
Mark Odell <dracone.cordis@gmail.com>
2014-10-25 15:21:36 -0700

    Added Stats manager and data model class.

---
+++ PhoneApp/FingerNinja/FingerNinja/app/src/main/java/edu/sjsu/techknowgeek/fingerninja/ScoreManager.java
@@ -0,0 +1,24 @@
+package edu.sjsu.techknowgeek.fingerninja;
+
+/**
+ *  Contains static data structure for scores. It allows for
+ *  other categories to be added should you want to increase the
+ *  number of categories of scores. Completely unnecessary for
+ *  this assignment.
+ *  @author Mark Odell
+ *  @version 25-10-2014
+ */
+public class ScoreManager {
+    // Categories
+    public enum Categories {
+        CURRENT_USER,
+        OVERALL_USERS
+    };
+
+    //     Current User
+    //     Overall Users
+    // List of Highscores
+    //public static lists = new (ArrayList<int>)[2];
+    //     User & Overall(out of all users) Scores
+    // SetGetMethods
+}
---
+++ PhoneApp/FingerNinja/FingerNinja/app/src/main/java/edu/sjsu/techknowgeek/fingerninja/StatsActivity.java
@@ -0,0 +1,12 @@
+package edu.sjsu.techknowgeek.fingerninja;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StatsActivity extends Fragment {
+
+}
