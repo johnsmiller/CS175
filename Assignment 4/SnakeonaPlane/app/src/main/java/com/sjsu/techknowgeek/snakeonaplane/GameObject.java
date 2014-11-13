f69c9e6b506b5a3c3ecd679372d895c9500e2eab
johnsmiller <computerman790@gmail.com>
2014-11-13 15:52:32 -0800

    GameObject

---
+++ Assignment 4/SnakeonaPlane/app/src/main/java/com/sjsu/techknowgeek/snakeonaplane/GameObject.java
@@ -0,0 +1,30 @@
+package com.sjsu.techknowgeek.snakeonaplane;
+
+/**
+ * Created by John on 11/13/2014.
+ */
+public class GameObject {
+    //direction
+        //0 = stationary
+        //1 = WEST
+        //2 = NORTH
+        //3 = EAST
+        //4 = SOUTH
+        int direction;
+    //speed
+        //increment by 1
+        int speed;
+    //location
+        int x;
+        int y;
+
+    public GameObject(int initDir, int initSpd, int initX, int initY)
+    {
+        direction = initDir;
+        speed = initSpd;
+        x = initX;
+        y = initY;
+    }
+
+    //public update
+}
