7a87bdb4b0deb75a7f8a50fd85b24a10fc917df3
Mark Odell <dracone.cordis@gmail.com>
2014-11-13 16:20:32 -0800

    Broken need help to figure out brokeness

--- Assignment 4/SnakeonaPlane/app/src/main/java/com/sjsu/techknowgeek/snakeonaplane/GameActivity.java
+++ Assignment 4/SnakeonaPlane/app/src/main/java/com/sjsu/techknowgeek/snakeonaplane/GameActivity.java
@@ -2,6 +2,10 @@ package com.sjsu.techknowgeek.snakeonaplane;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,11 +13,38 @@ import android.view.MenuItem;
 
 public class GameActivity extends Activity {
 
+    private GLSurfaceView mGLSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new SnakeRender());
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
         ActionBar actionBar = getActionBar();
         if(actionBar != null)
             actionBar.hide();
@@ -40,5 +71,21 @@ public class GameActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        //super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
     }
 }
