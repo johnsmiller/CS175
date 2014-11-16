f826751e1ab744d2abf27be926dc5b59636bb988
Mark Odell <dracone.cordis@gmail.com>
2014-11-16 15:31:46 -0800

    Changed SnakeRender to have some vertices to drawn our walls and snake head.

--- Assignment 4/SnakeonaPlane/app/src/main/java/com/sjsu/techknowgeek/snakeonaplane/SnakeRender.java
+++ Assignment 4/SnakeonaPlane/app/src/main/java/com/sjsu/techknowgeek/snakeonaplane/SnakeRender.java
@@ -43,9 +43,9 @@ public class SnakeRender implements GLSurfaceView.Renderer {
     private float[] mMVPMatrix = new float[16];
 
     /** Store our model data in a float buffer. */
-    private final FloatBuffer mTriangle1Vertices;
-    private final FloatBuffer mTriangle2Vertices;
-    private final FloatBuffer mTriangle3Vertices;
+    private final FloatBuffer mSnakeVertices;
+    private final FloatBuffer mWallVertices;
+
 
     /** This will be used to pass in the transformation matrix. */
     private int mMVPMatrixHandle;
@@ -60,7 +60,7 @@ public class SnakeRender implements GLSurfaceView.Renderer {
     private final int mBytesPerFloat = 4;
 
     /** How many elements per vertex. */
-    private final int mStrideBytes = 7 * mBytesPerFloat;
+    private final int mStrideBytes = 7 * mBytesPerFloat; // Eight floats for a game object
 
     /** Offset of the position data. */
     private final int mPositionOffset = 0;
@@ -74,6 +74,12 @@ public class SnakeRender implements GLSurfaceView.Renderer {
     /** Size of the color data in elements. */
     private final int mColorDataSize = 4;
 
+    /** Offset of game object **/
+    private final int mGameTypeOffset = 7;
+
+    /** Size of the game object data in elements. **/
+    private final int mGameTypeSize = 1;
+
     /**
      * Initialize the model data.
      */
@@ -82,70 +88,80 @@ public class SnakeRender implements GLSurfaceView.Renderer {
         // Define points for equilateral triangles.
 
         // This triangle is red, green, and blue.
-        final float[] triangle1VerticesData = {
+        final float[] snake = {
                 // X, Y, Z,
                 // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 0.0f, 0.0f, 1.0f,
+                0.0f, -0.5f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
 
-                0.5f, -0.25f, 0.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
+                0.5f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
 
-                0.0f, 0.559016994f, 0.0f,
-                0.0f, 1.0f, 0.0f, 1.0f};
+                0.5f, 0.5f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+
+                -0.5f, 0.5f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+
+                -0.5f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+        };
+
+        final float[] tail = {
+                // X, Y, Z,
+                // R, G, B, A
+                -0.25f, -0.25f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+
+                0.25f, -0.25f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+
+                0.25f, 0.25f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+
+                -0.25f, 0.25f, 0.0f,
+                0.0f, 1.0f, 0.0f, 1.0f,
+        };
 
         // This triangle is yellow, cyan, and magenta.
-        final float[] triangle2VerticesData = {
+        final float[] wall = {
                 // X, Y, Z,
                 // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 1.0f, 0.0f, 1.0f,
-
-                0.5f, -0.25f, 0.0f,
-                0.0f, 1.0f, 1.0f, 1.0f,
-
-                0.0f, 0.559016994f, 0.0f,
-                1.0f, 0.0f, 1.0f, 1.0f};
-
-        // This triangle is white, gray, and black.
-        final float[] triangle3VerticesData = {
-                // X, Y, Z,
-                // R, G, B, A
-                -0.5f, -0.25f, 0.0f,
-                1.0f, 1.0f, 1.0f, 1.0f,
-
-                0.5f, -0.25f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
                 0.5f, 0.5f, 0.5f, 1.0f,
 
-                0.0f, 0.559016994f, 0.0f,
-                0.0f, 0.0f, 0.0f, 1.0f};
+                0.5f, -0.5f, 0.0f,
+                0.5f, 0.5f, 0.5f, 1.0f,
+
+                0.5f, 0.5f, 0.0f,
+                0.5f, 0.5f, 0.5f, 1.0f,
+
+                -0.5f, 0.5f, 0.0f,
+                0.5f, 0.5f, 0.5f, 1.0f,
+        };
 
         // Initialize the buffers.
-        mTriangle1Vertices = ByteBuffer.allocateDirect(
-                triangle1VerticesData.length * mBytesPerFloat)
+        mSnakeVertices = ByteBuffer.allocateDirect(
+                snake.length * mBytesPerFloat)
                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mTriangle2Vertices = ByteBuffer.allocateDirect(
-                triangle2VerticesData.length * mBytesPerFloat)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        mTriangle3Vertices = ByteBuffer.allocateDirect(
-                triangle3VerticesData.length * mBytesPerFloat)
+        mWallVertices = ByteBuffer.allocateDirect(
+                wall.length * mBytesPerFloat)
                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
 
-        mTriangle1Vertices.put(triangle1VerticesData).position(0);
-        mTriangle2Vertices.put(triangle2VerticesData).position(0);
-        mTriangle3Vertices.put(triangle3VerticesData).position(0);
+        mSnakeVertices.put(snake).position(0);
+        mWallVertices.put(wall).position(0);
     }
 
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config)
     {
         // Set the background clear color to gray.
-        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 0.5f);
 
         // Position the eye behind the origin.
         final float eyeX = 0.0f;
         final float eyeY = 0.0f;
-        final float eyeZ = 1.5f;
+        final float eyeZ = 10.0f;
 
         // We are looking toward the distance
         final float lookX = 0.0f;
@@ -319,48 +335,40 @@ public class SnakeRender implements GLSurfaceView.Renderer {
     {
         GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
 
-        // Do a complete rotation every 10 seconds.
-        long time = SystemClock.uptimeMillis() % 10000L;
-        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
+//        // Do a complete rotation every 10 seconds.
+//        long time = SystemClock.uptimeMillis() % 10000L;
+//        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
 
         // Draw the triangle facing straight on.
         Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-        drawTriangle(mTriangle1Vertices);
+        //Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        drawGameObject(mSnakeVertices, 5);
 
         // Draw one translated a bit down and rotated to be flat on the ground.
         Matrix.setIdentityM(mModelMatrix, 0);
         Matrix.translateM(mModelMatrix, 0, 0.0f, -1.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-        drawTriangle(mTriangle2Vertices);
-
-        // Draw one translated a bit to the right and rotated to be facing to the left.
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 1.0f, 0.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, 90.0f, 0.0f, 1.0f, 0.0f);
-        Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
-        drawTriangle(mTriangle3Vertices);
+        //Matrix.rotateM(mModelMatrix, 0, 90.0f, 1.0f, 0.0f, 0.0f);
+        //Matrix.rotateM(mModelMatrix, 0, angleInDegrees, 0.0f, 0.0f, 1.0f);
+        drawGameObject(mWallVertices, 4);
     }
 
     /**
-     * Draws a triangle from the given vertex data.
      *
-     * @param aTriangleBuffer The buffer containing the vertex data.
+     * @param aGameBuff The buffer containing the vertex data.
      */
-    private void drawTriangle(final FloatBuffer aTriangleBuffer)
+    private void drawGameObject(final FloatBuffer aGameBuff, int numOfVertices)
     {
         // Pass in the position information
-        aTriangleBuffer.position(mPositionOffset);
+        aGameBuff.position(mPositionOffset);
         GLES20.glVertexAttribPointer(mPositionHandle, mPositionDataSize, GLES20.GL_FLOAT, false,
-                mStrideBytes, aTriangleBuffer);
+                mStrideBytes, aGameBuff);
 
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
         // Pass in the color information
-        aTriangleBuffer.position(mColorOffset);
+        aGameBuff.position(mColorOffset);
         GLES20.glVertexAttribPointer(mColorHandle, mColorDataSize, GLES20.GL_FLOAT, false,
-                mStrideBytes, aTriangleBuffer);
+                mStrideBytes, aGameBuff);
 
         GLES20.glEnableVertexAttribArray(mColorHandle);
 
@@ -374,7 +382,7 @@ public class SnakeRender implements GLSurfaceView.Renderer {
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
 
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, numOfVertices);
     }
 }
 
