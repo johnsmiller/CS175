be55728adc82a840992e8cd6659a2d4c726a79ab
johnsmiller <computerman790@gmail.com>
2014-12-07 23:42:05 -0800

    Muhahahaha! Location Aware Baby! Aw Yeah!

--- WeatherApp/app/src/main/java/com/sjsu/techknowgeek/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/sjsu/techknowgeek/weatherapp/MainActivity.java
@@ -1,19 +1,83 @@
 package com.sjsu.techknowgeek.weatherapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener{
+
+    private LocationClient mLocationClient;
+    private ProgressBar mActivityIndicator;
+
+    private TextView mCityTextView;
+    private TextView mTemperatureTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hide action bar
+        ActionBar bar = getActionBar();
+        if(bar != null)
+            bar.hide();
+
+        mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
+
+        //start location update
+        mLocationClient = new LocationClient(this, this, this);
+        mCityTextView = (TextView) findViewById(R.id.currentLocation);
+        mTemperatureTextView = (TextView) findViewById(R.id.tempValue);
+
+        //load previous location from SQL database here
+        //TODO: Load previous location
+
+        //TODO: We need a GUI component that makes use of touch events (Refresh location button?? C/F degrees? Refresh Weather?)
     }
 
+    private void getWeather(String postalCode)
+    {
+        //Just to show that it works for now
+        Toast.makeText(this, "Postal Code: " + postalCode, Toast.LENGTH_LONG).show();
+        //Download weather from internet
+        //parse file
+        //update text view with weather
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +99,79 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "Location Connected. Updating Weather Info.", Toast.LENGTH_SHORT).show();
+        Location mCurrentLocation = mLocationClient.getLastLocation();
+        getAddress(mCurrentLocation);
+
+    }
+
+    @Override
+    public void onDisconnected() {
+        Toast.makeText(this, "Disconnected from location. Please re-connect.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Location connection has failed :(", Toast.LENGTH_SHORT).show();
+    }
+
+    public void getAddress(Location mLocation) {
+        // Ensure that a Geocoder services is available
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD && Geocoder.isPresent()) {
+            // Show the activity indicator
+            mActivityIndicator.setVisibility(View.VISIBLE);
+            (new GetAddressTask(this)).execute(mLocation);
+        }
+    }
+
+    private class GetAddressTask extends AsyncTask<Location, Void, String> {
+        Context mContext;
+        public GetAddressTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected String doInBackground(Location... params) {
+            Geocoder geocoder =
+                    new Geocoder(mContext, Locale.getDefault());
+            // Get the current location from the input parameter list
+            Location loc = params[0];
+
+            List<Address> addresses = null;
+
+            try {
+                addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e1) {
+                return ("IO Exception trying to get address");
+            } catch (IllegalArgumentException e2) {
+                return "Bad Arguments passed to address service";
+            }
+
+            // If the reverse geocode returned an address
+            if (addresses != null && addresses.size() > 0) {
+                // Get the first address
+                final Address address = addresses.get(0);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mCityTextView.setText(address.getLocality());
+                    }
+                });
+                return address.getPostalCode();
+            } else {
+                return "No address found";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String postalCode) {
+            mActivityIndicator.setVisibility(View.GONE);
+            getWeather(postalCode);
+        }
     }
 }
